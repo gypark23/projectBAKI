31c73a9cd52ead17eca68553b0803546ccf21a0f
austinmei5 <114614372+austinmei5@users.noreply.github.com>
2022-10-17 18:27:27 -0500

    started set 2 front end shit

---
+++ test2_set2/test2_set2_instructions.js
@@ -0,0 +1,24 @@
+function like() {
+    navigator.vibrate([150]);
+    buttonPressed = true;
+}
+function love() {
+    navigator.vibrate([150, 100, 150]);
+    buttonPressed = true;
+}
+function laugh() {
+    navigator.vibrate([150,100,150,100,150,100,150]);
+    buttonPressed = true;
+}
+function care() {
+    navigator.vibrate([80,50,80,50,200]);
+    buttonPressed = true;
+}
+function sad() {
+    navigator.vibrate([200]);
+    buttonPressed = true;
+}
+function angry() {
+    navigator.vibrate([200,80,200]);
+    buttonPressed = true;
+}
