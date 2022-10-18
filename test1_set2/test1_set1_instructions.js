6b0818cbb03b0ba087335bcc8280ddd14400248a
austinmei5 <114614372+austinmei5@users.noreply.github.com>
2022-10-17 23:23:36 -0500

    updated vibrations again

--- test1_set2/test1_set1_instructions.js
+++ test1_set2/test1_set1_instructions.js
@@ -7,18 +7,18 @@ function love() {
     buttonPressed = true;
 }
 function laugh() {
-    navigator.vibrate([300,100,150,100,300]);
+    navigator.vibrate([150,100,150,100,150,100,150]);
     buttonPressed = true;
 }
 function care() {
-    navigator.vibrate([150,100,150,100,300,100,300]);
+    navigator.vibrate([150,100,150,100,300]);
     buttonPressed = true;
 }
 function sad() {
-    navigator.vibrate([300,100,300]);
+    navigator.vibrate([300]);
     buttonPressed = true;
 }
 function angry() {
-    navigator.vibrate([300,100,300,100,300,100,300]);
+    navigator.vibrate([300,100,300]);
     buttonPressed = true;
 }
