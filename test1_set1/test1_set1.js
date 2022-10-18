b7ead80785f8f558f4aa759ce4d7a99d37fc745d
Kyu Park <gypark23@uchicago.edu>
2022-10-17 21:28:58 -0500

    test result added

--- test1_set1/test1_set1.js
+++ test1_set1/test1_set1.js
@@ -31,44 +31,46 @@ var reactionNumber = 0;
 var buttonPressed = -1;
 var input = 0;
 console.log("hello");
-var testNo = 1;
+var testNo = 0;
 var firstRun = true;
 var vibrationDone = false;
 
 function test(num) {
     buttonPressed = parseInt(num);
     if (vibrationDone) {
-        if (buttonPressed == reactionNumber) {
+        if (buttonPressed == reactionNumber && testNo != 0) {
             correct++;
         }
         testNo++;
         buttonPressed = -1;
         vibrationDone = false;
-        document.getElementById("selectNow").textContent="Wait...";
-        if (testNo <= totalTest) {
-            document.getElementById("testText").textContent=`Test ${testNo} of 10: Select the correct emoticon below`;
+        document.getElementById("selectNow").textContent = "Wait...";
+            document.getElementById("testText").textContent = `Test ${testNo} of 10: Select the correct emoticon below`;
             executeTest();
-        }
     }
-    
+
 }
 
 function playVibration() {
     map[reactionNumber]();
     console.log("vibration done");
     vibrationDone = true;
-    document.getElementById("selectNow").textContent="You may select now!";
+    document.getElementById("selectNow").textContent = "You may select now!";
 }
 function executeTest() {
-    if (testNo <= totalTest) {
+    if (firstRun && buttonPressed != -1) {
+        vibrationDone = true;
+        firstRun = false;
+    }
+    else if (testNo <= totalTest) {
         //random reaction number
         reactionNumber = Math.floor(Math.random() * 6);
         //wait 2 seconds to play the vibration
-        console.log(`start${testNo}`);
+        console.log(`start${testNo} ${reactionNumber}`);
         setTimeout(playVibration, 2000);
     }
     else {
-
+        alert(`Test done!\nPress OK to move on to the next experiment!\nFor BAKI: Test1_set1 ${correct}`);
     }
 }
 
