ec157104967b70de72eacb3225e5e33a6c542312
Kyu Park <gypark23@uchicago.edu>
2022-10-19 14:12:17 -0500

    useless changes

--- test2_gestures/gestures_set2.js
+++ test2_gestures/gestures_set2.js
@@ -1,3 +1,44 @@
+
+
+
+touchableElement.addEventListener('touchstart', function (event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+}, false);
+
+touchableElement.addEventListener('touchend', function (event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture();
+}, false);
+
+
+function handleGesture() {
+    if (touchendX < touchstartX) {
+        console.log('Swiped Left');
+    }
+
+    if (touchendX > touchstartX) {
+        console.log('Swiped Right');
+    }
+
+    if (touchendY < touchstartY) {
+        console.log('Swiped Up');
+    }
+
+    if (touchendY > touchstartY) {
+        console.log('Swiped Down');
+    }
+
+    if (touchendY === touchstartY) {
+        console.log('Tap');
+    }
+}
+
+
+
+
+/*
 const topLeft = document.getElementById("top-left")
 const topRight = document.getElementById("top-right")
 const bottomLeft = document.getElementById("bottom-left")
@@ -46,7 +87,7 @@ document.addEventListener("touchend", e => {
         dot.remove()
     })
 })
-
+*/
 
 
 
